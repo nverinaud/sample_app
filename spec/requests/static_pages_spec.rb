d09ba4fb5416e085eaf802ed5feceda9bc7e12f4
Nicolas VERINAUD <n.verinaud@gmail.com>
2012-02-15 17:25:12 +0100

    Update tests & sign up page coherency.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,31 +4,67 @@ describe "Static pages" do
 
   subject { page }
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    page.should have_selector 'title', text: full_title('Home')
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign Up')
+  end
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(pageTitle)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1',    text: 'Sample App') }
-    it { should have_selector('title', text: full_title('Home')) }
+    let(:heading) { 'Sample App' }
+    let(:pageTitle) { 'Home' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_selector('h1',    text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
+    let(:heading) { 'Help' }
+    let(:pageTitle) { 'Help' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_selector('h1',    text: 'About') }
-    it { should have_selector('title', text: full_title('About Us')) }
+    let(:heading) { 'About' }
+    let(:pageTitle) { 'About Us' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_selector('h1',    text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
+    let(:heading) { 'Contact' }
+    let(:pageTitle) { 'Contact' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe "Sign Up page" do
+    before { visit signup_path }
+
+    let(:heading) { 'Sign Up' }
+    let(:pageTitle) { 'Sign Up' }
+
+    it_should_behave_like "all static pages"
   end
 end

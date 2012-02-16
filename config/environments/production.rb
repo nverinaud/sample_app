5392870814271e2a664a89d515a121f0c0ddd890
Nicolas VERINAUD <n.verinaud@gmail.com>
2012-02-16 17:18:19 +0100

    Revert SSL.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,5 +34,5 @@ SampleApp::Application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 end

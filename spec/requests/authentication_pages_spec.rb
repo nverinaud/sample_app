d3905788ff9b17ea4fd87b2d18b5124941d32ee0
Nicolas VERINAUD <n.verinaud@gmail.com>
2012-02-22 23:14:08 +0100

    Do ex 9.6.7

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -110,6 +110,14 @@ describe "Authentication" do
           it "should render the desired protected page" do
             page.should have_title('Edit user')
           end
+
+          describe "when signing in again" do
+            before { sign_in user }
+            it "should render the default (profile) page" do
+              page.should have_title(user.name)
+            end
+          end
+
         end
       end
     end

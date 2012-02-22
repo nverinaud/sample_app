6684d339a01e8b62482b571e11c58c967c2fbab2
Nicolas VERINAUD <n.verinaud@gmail.com>
2012-02-22 22:35:12 +0100

    Fix a bug which caused a user to be signed out after changing his own settings.

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,7 @@ class User < ActiveRecord::Base
   private
 
     def create_remember_token
-      self.remember_token = SecureRandom.urlsafe_base64
+      self.remember_token ||= SecureRandom.urlsafe_base64
     end
 
 end

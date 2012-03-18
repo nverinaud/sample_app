bcbcca58e7261b6ef4e73611d752c394e61e5d90
Nicolas VERINAUD <n.verinaud@gmail.com>
2012-03-18 17:29:12 +0100

    Do Exercise 1.0.5.8

---
+++ app/assets/javascripts/microposts.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+
+	$("textarea#micropost-text-area").bind('textchange', function(){
+		var content = $(this).val();
+		var charsLeft = 140-parseInt(content.length);
+		countSpan = $("span#micropost-chars-count");
+		countSpan.text(charsLeft);
+		if (charsLeft >= 0)
+			countSpan.removeClass('error').addClass('success');
+		else
+			countSpan.removeClass('success').addClass('error');
+	});
+
+});

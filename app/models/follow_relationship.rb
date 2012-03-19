0c9608351c69b4a571fcb86e3bcea9776934d30b
Nicolas VERINAUD <n.verinaud@gmail.com>
2012-03-19 15:29:21 +0100

    Add attr_accessible for FollowRelationship model.

--- app/models/follow_relationship.rb
+++ app/models/follow_relationship.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: follow_relationships
+#
+#  id          :integer         not null, primary key
+#  follower_id :integer
+#  followed_id :integer
+#  created_at  :datetime        not null
+#  updated_at  :datetime        not null
+#
+
+#
+# follower_id follows followed_id
+# get Doe's feed : all followed_id where follower_id = doe_id
+# get Doe's followers : all follower_id where followed_id = doe_id
+#
+
 class FollowRelationship < ActiveRecord::Base
+	attr_accessible :followed_id
 end
--- spec/models/follow_relationship_spec.rb
+++ spec/models/follow_relationship_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: follow_relationships
+#
+#  id          :integer         not null, primary key
+#  follower_id :integer
+#  followed_id :integer
+#  created_at  :datetime        not null
+#  updated_at  :datetime        not null
+#
+
 require 'spec_helper'
 
 describe FollowRelationship do
